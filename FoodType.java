cf0f7224e5ea63646ceef5c0e87d7434f1f55cd3
Hew Kai Feng <khew0006@student.monash.edu>
2025-06-18 20:06:52 +0800

    Add files via upload

---
+++ FoodType.java
@@ -0,0 +1,13 @@
+/**
+ * This enum represents the available food type that will be
+ * appeared in the menu for each customer which is pizza or pasta only.
+ *
+ * @author Hew Kai Feng
+ * @version ver 1.3.3
+ */
+
+public enum FoodType
+{
+    PIZZA,
+    PASTA
+}
